eee1c923fd815775aa7784c85a1944be637b9a0c
Benhard Sim <36811298+BenhardSim@users.noreply.github.com>
2023-05-25 23:22:37 +0700

    add recycleview grid

---
+++ app/src/main/java/com/example/promosee/adapter/GridSpacingItemDecoration.kt
@@ -0,0 +1,15 @@
+package com.example.promosee.adapter
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class GridSpacingItemDecoration(private val spanCount: Int, private val spacing: Int, private val includeEdge: Boolean) : RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        outRect.left = spacing
+        outRect.right = spacing
+        outRect.top = spacing
+        outRect.bottom = spacing
+    }
+}
